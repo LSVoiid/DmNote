6caf1f4154eca30985ab175dfa65b5498ea712af
lee-sihun <cnsa201119@gmail.com>
2025-12-03 10:59:42 +0900

    docs: 프로그램, css 가이드 추가

--- docs/plugin/_meta.js
+++ docs/plugin/_meta.js
@@ -1,4 +1,20 @@
 export default {
+  index: {
+    title: "소개",
+    theme: {
+      breadcrumb: false,
+    },
+  },
+  "---guide": {
+    type: "separator",
+    title: "기본 가이드",
+  },
+  guide: "프로그램 가이드",
+  "custom-css": "커스텀 CSS",
+  "---plugin": {
+    type: "separator",
+    title: "플러그인 시스템",
+  },
   "getting-started": "시작하기",
   "declarative-api": "선언형 API",
   "template-syntax": "템플릿 문법",
